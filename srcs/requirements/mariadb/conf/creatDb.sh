ae1a2bf78fa5d689cfd785bd5ebeddb83e9ae298
Yousra Ibarki <yoibarki@e2r1p1.1337.ma>
2024-06-06 13:31:40 +0100

    502 bad getaway

--- srcs/requirements/mariadb/conf/creatDb.sh
+++ srcs/requirements/mariadb/conf/creatDb.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-#db_name = Database Name
-#db_user = User
-#db_pwd = User Password
-
-# echo "CREATE DATABASE IF NOT EXISTS wordpress ;" > db1.sql
-# echo "CREATE USER IF NOT EXISTS 'yoibarki'@'%' IDENTIFIED BY '1337' ;" >> db1.sql
-# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'yoibarki'@'%' ;" >> db1.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-# echo "FLUSH PRIVILEGES;" >> db1.sql
-
-# mysql < db1.sql
-
-# /usr/bin/mysqld_safe
-
-#!/bin/bash
-
-#!/bin/bash
-
 service mariadb start 
+
 sleep 5 
 
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,17 +4,17 @@
 
 # curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /usr/local/bin/wp \
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-chmod +x /usr/local/bin/wp
+# chmod +x /usr/local/bin/wp
 
-mv wp-cli.phar /usr/local/bin/wp
+# mv wp-cli.phar /usr/local/bin/wp
 
-cd /var/www/wordpress
+# cd /var/www/wordpress
 
-chmod -R 755 /var/www/wordpress/
+# chmod -R 755 /var/www/wordpress/
 
-chown -R www-data:www-data /var/www/wordpress
+# chown -R www-data:www-data /var/www/wordpress
 
 # # check if mariadb container is up and running
 # ping_mariadb_container() {
@@ -41,18 +41,37 @@ chown -R www-data:www-data /var/www/wordpress
 
 
 
+# wp core download --path=/var/www/html --allow-root
+
+# wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+
+# wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
+
+# wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
+
+# sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+
+
+
+# mkdir -p /run/php
+
+# php-fpm7.4 -F
+
+
+sed -i -e 's/listen =.*/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+
+# download core files
 wp core download --path=/var/www/html --allow-root
 
-wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+# Create WordPress config file
+wp config create --dbhost=mariadb --dbname=${MYSQL_DB_NAME} --dbuser=${MYSQL_USR} --dbpass=${MYSQL_PWD} --path=/var/www/html --allow-root --skip-check
 
-wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
+# Install WordPress and create admin user
+wp core install --url=${DOMAINE_NAME} --title="Wordpress page" --admin_name=${WP_USR} --admin_password=${WP_PWD} --admin_email="${ADMIN_EMAIL}"  --path=/var/www/html --allow-root
 
-wp user create "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
+# create second user
+wp user create "${USER}" "${USER_EMAIL}" --user_pass=${WP_PWD}  --allow-root --path=/var/www/html
 
-sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
-
-
-
+# Start PHP-FPM
 mkdir -p /run/php
-
 php-fpm7.4 -F
